831cc38cb026bc68701958e100e94c858e112183
Henry <li.henry.hfl@gmail.com>
2021-06-19 14:15:56 +0000

    fixed setup script

--- mhcflurry_setup.sh
+++ mhcflurry_setup.sh
@@ -1,8 +1,13 @@
+#!/bin/bash
+
 python -m venv mhcflurry_env
 source ./mhcflurry_env/bin/activate
-python setup.py install --user
-pip install tensorflow==2.3.0
-pip install tensorflow-gpu==2.3.0
+python -m pip install --upgrade pip setuptools cython pandas
+python setup.py install
+python -m pip install tensorflow==2.3.0
+python -m pip install tensorflow-gpu==2.3.0
 mhcflurry-downloads fetch
 mhcflurry-downloads fetch allele_sequences
 mhcflurry-downloads fetch random_peptide_predictions
+python -m pip install ipykernel
+python -m ipykernel install --user --name=mhcflurry_env
